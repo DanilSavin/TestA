c85b9be479c7777c3f2dc08e84dc4a5d85d01b09
DanilSavin <105873682+DanilSavin@users.noreply.github.com>
2023-10-26 17:21:56 +0300

    task 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,8 +2,8 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        int a = 5;
-        int b = 6;
+        int a = 10;
+        int b = 15;
         Calculate(a,b);
     }
     public static int Calculate (int a, int b){
