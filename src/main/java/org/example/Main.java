f6a60484e1e78329a9877262920472b8d950a953
DanilSavin <105873682+DanilSavin@users.noreply.github.com>
2023-10-27 10:22:05 +0300

    task 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,27 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         int a = 6;
         int b = 8;
-        System.out.println(Calculate(a,b));
-        System.out.println(StringName(a,b));
+        Scanner sc = new Scanner(System.in);
+        String symbol = sc.nextLine();
+        System.out.println(Calculate(a,b,symbol));
     }
-    public static int Calculate (int a, int b){
-        return a - b;
+    public static int Calculate (int a, int b,String symbol){
+        if (symbol.equals(Symbol.Plus)){
+            return a + b;
+        } if (symbol.equals(Symbol.Minus)){
+            return a - b;
+        } if (symbol.equals(Symbol.Delete)){
+            return a / b;
+        } if (symbol.equals(Symbol.Umnozhit)) {
+            return a * b;
+        }
+        return 0;
     }
-    public static String StringName (int a, int b){
-        return Calculate(a,b) >=0 ? "положительный":"отрицательный";
-    }
+
 
 }
