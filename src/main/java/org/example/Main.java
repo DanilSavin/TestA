672894bb827c0f1d0ad8aa57723b757fb29f0134
DanilSavin <105873682+DanilSavin@users.noreply.github.com>
2023-10-27 10:09:04 +0300

    task 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,11 +2,16 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        int a = 5;
-        int b = 6;
-        Calculate(a,b);
+        int a = 6;
+        int b = 8;
+        System.out.println(Calculate(a,b));
+        System.out.println(StringName(a,b));
     }
     public static int Calculate (int a, int b){
-        return a + b;
+        return a - b;
     }
+    public static String StringName (int a, int b){
+        return Calculate(a,b) >=0 ? "положительный":"отрицательный";
+    }
+
 }
