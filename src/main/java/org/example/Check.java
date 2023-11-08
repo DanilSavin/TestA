06036a24cb18ae98e461086de513bab4aeb43e33
DanilSavin <105873682+DanilSavin@users.noreply.github.com>
2023-11-08 21:36:28 +0300

    task_4_1 - до работал программу

--- src/main/java/org/example/Check.java
+++ src/main/java/org/example/Check.java
@@ -1,8 +1,8 @@
 package org.example;
 
 public class Check {
-    public static void checkNumber(int a) {
-        if (a % 2 == 0) {
+    public static void checkNumber(int b) {
+        if (b % 2 == 0) {
             System.out.println("Четная цифра");
         } else {
             System.out.println("Не четная цифра");
